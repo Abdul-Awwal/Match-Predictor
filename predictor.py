b24b1297641589febca4c787aa492fcbd2fa4d73
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-12-03 11:55:51 -0600

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -5,12 +5,18 @@ import numpy as np
 import time
 from dask import dataframe as df1
 
-# time taken to read data
-s_time_dask = time.time()
-dask_df = df1.read_csv('dataset.csv')
-e_time_dask = time.time()
+def main(home_team, away_team, week):
+    # time taken to read data
+    s_time_dask = time.time()
+    dask_df = df1.read_csv('eplmatches.csv')
+    e_time_dask = time.time()
+    
+    print("Read with dask: ", (e_time_dask-s_time_dask), "seconds")
+    print(home_team, away_team, week)
+    
+    # data
+    dask_df.head(10)
+    
 
-print("Read with dask: ", (e_time_dask-s_time_dask), "seconds")
-
-# data
-dask_df.head(10)
+if __name__ == "__main__":
+    main("Liverpool", "Chelsea", 4)
